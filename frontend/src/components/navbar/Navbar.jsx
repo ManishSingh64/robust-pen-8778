ee14390560c4004546d61884af1c39214dbdd090
roy897 <roushangupta85@gmail.com>
2022-09-29 15:36:55 +0530

    navbar added

---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { Notification } from "./notifications/Notification";
+
+import { Button, Flex, Image } from "@chakra-ui/react";
+import { UserDetails } from "./userDetails/UserDetails";
+
+export const Navbar = () => {
+  return (
+    <>
+      <Flex gap='5'>
+        <Button variant='outline' borderColor='blue' color='blue' size='sm'>UPGRADE</Button>
+        <Notification/>
+        <UserDetails/>
+      </Flex>
+    </>
+  );
+};
